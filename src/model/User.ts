2daa0fbe5b6f467892d242a09597f309593e1a86
ykc <ykilicarslan@sabanciuniv.edu>
2023-04-07 21:36:39 +0300

    init

---
+++ src/model/User.ts
@@ -0,0 +1,15 @@
+// models/User.js
+
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  first_name: String,
+  last_name: String,
+  email: String,
+  password: String,
+  token: String,
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
