be8d09717a6791dfca4768d5ca2b42ed71561eb1
ykc <ykilicarslan@sabanciuniv.edu>
2023-04-14 21:12:30 +0300

    error page and register

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -2,7 +2,6 @@ import CategoryPreview from "../components/home/CategoryPreview";
 import LandingView from "../components/home/LandingView";
 import { CategoryPreviewList } from "../types/categoryPreviewType";
 import { GetStaticProps } from "next";
-import axios, { AxiosResponse } from "axios";
 
 export default function Home({ categoryPreviewList }: CategoryPreviewList) {
     return (
