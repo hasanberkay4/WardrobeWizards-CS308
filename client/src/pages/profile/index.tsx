64c294a7f49cff8d7a18cd7d8557cb17bd0ce6a1
ykc <ykilicarslan@sabanciuniv.edu>
2023-05-05 16:22:00 +0300

    profile userInfo

--- client/src/pages/profile/index.tsx
+++ client/src/pages/profile/index.tsx
@@ -1,6 +1,8 @@
 import { GetServerSideProps } from 'next';
 import ProfileHome, { ProfileHomeProps } from '../../components/profile/profileHome';
 import ProfileNav from '../../components/profile/profileNav';
+import jwt_decode from 'jwt-decode';
+import cookie from 'cookie';
 
 const ProfilePage = ({ profileInfo }: ProfileHomeProps) => {
     return (
@@ -14,11 +16,33 @@ const ProfilePage = ({ profileInfo }: ProfileHomeProps) => {
 
 export const getServerSideProps: GetServerSideProps<ProfileHomeProps> = async (context) => {
     // get user profile info
-    // do stuff 
+    const { req } = context;
 
-    const profileInfo = {
-        name: "John Doe"
+    // Get the authentication token from cookies
+    const cookies = cookie.parse(req.headers.cookie || '');
+    const authToken = cookies['token'];
+
+    if (!authToken) {
+        console.error('Authentication token is missing');
+        return {
+            props: {
+                profileInfo: [], // return an empty deliveries array in case of missing token
+            },
+        };
     }
+
+    const decodedToken = jwt_decode(authToken) as { user_id: string; };
+    const userId = decodedToken.user_id;
+    console.log("usedId", userId);
+
+    const user = await fetch(`http://localhost:5001/users/user/${userId}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+    const profileInfo = await user.json()
+    console.log("profileInfo", profileInfo);
 
     return {
         props: {
