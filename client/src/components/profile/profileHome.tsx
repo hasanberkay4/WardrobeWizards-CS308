d51eb10080b223d08d27910bffab835475160597
Koray <korayaslan365@gmail.com>
2023-06-11 17:29:35 +0300

    style of the waleet has been changed

--- client/src/components/profile/profileHome.tsx
+++ client/src/components/profile/profileHome.tsx
@@ -1,4 +1,3 @@
-
 export type ProfileHomeProps = {
     profileInfo: {
         name: string,
@@ -8,10 +7,14 @@ export type ProfileHomeProps = {
 
 export default function ProfileHome({ profileInfo }: ProfileHomeProps) {
     return (
-        <div>
-            <h1>Profile Home</h1>
-            <h2>Name: {profileInfo.name}</h2>
-            <h2>Wallet: ₺{profileInfo.wallet}</h2>
+        <div className="mt-4 ml-4">
+            <div className="max-w-lg bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+                <div className="p-8">
+                    <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{profileInfo.name}</div>
+                    <h2 className="block mt-1 text-lg leading-tight font-medium text-black">Wallet Balance</h2>
+                    <p className="mt-2 text-gray-500 text-xl">₺{profileInfo.wallet}</p>
+                </div>
+            </div>
         </div>
     )
 }
